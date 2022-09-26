2ec56960df6a5dde4646e7a0801f50dff37e0044
Ritav Kashyap <ritav20-237@aec.ac.in>
2022-09-26 11:57:52 +0530

    updated

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./MyComponents/About"
+import Achievements from "./MyComponents/Achievements"
+import Contact from "./MyComponents/Contact"
+import Experience from "./MyComponents/Experience"
+import Navbar from "./MyComponents/Navbar"
+import Projects from "./MyComponents/Projects"
+import Skills from "./MyComponents/Skills"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar/>;
+      <About/>;
+      <Skills/>;
+      <Achievements/>;
+      <Projects/>;
+      <Experience/>;
+      <Contact/>;
+    </main>
   );
 }
 
